e9c9b190f434116a1a42113726ce1c3046fa9605
Ivan-Khomutovskiy <62424190+Ivan-Khomutovskiy@users.noreply.github.com>
2020-08-10 11:34:10 +0200

    Add firebase Api

---
+++ functions/src/index.ts
@@ -0,0 +1,94 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as uuid from "uuid";
+
+admin.initializeApp();
+const db = admin.firestore();
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const greeter = functions.https.onRequest((request, response) => {
+  response.send("Greetings, " + request.body);
+});
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Kwooks!");
+});
+
+export const createUserAccount = functions.https.onRequest(
+  async (request, response) => {
+    const token = uuid.v4();
+    const user = await db
+      .collection("users")
+      .add({ tokens: [token], library: [] });
+    response.json({
+      token: token,
+      id: user.id,
+    });
+  }
+);
+
+export const addQuote = functions.https.onRequest(async (request, response) => {
+  const quote = request.body.quote;
+  const isbn = request.body.isbn;
+  const token = request.body.token;
+  const result = await db
+    .collection("users")
+    .where("tokens", "array-contains", token)
+    .get();
+  if (result.empty) {
+    response.status(401);
+    response.end();
+    return;
+  }
+  const user = result.docs[0];
+  await db.collection("quotes").add({ isbn, quote, userID: user.id });
+  response.end();
+});
+
+export const addToLibrary = functions.https.onRequest(
+  async (request, response) => {
+    const isbn = request.body.isbn;
+    const token = request.body.token;
+    const state = request.body.state;
+
+    const result = await db
+      .collection("users")
+      .where("tokens", "array-contains", token)
+      .get();
+    if (result.empty) {
+      response.status(401);
+      response.end();
+      return;
+    }
+    const user = result.docs[0];
+
+    await user.ref.collection("library").doc(isbn).set({"state": state});
+
+    response.end();
+  }
+);
+export const deleteBookFromLibrary = functions.https.onRequest(
+    async (request, response) => {
+      const isbn = request.body.isbn;
+      const token = request.body.token;
+      const result = await db
+        .collection("users")
+        .where("tokens", "array-contains", token)
+        .get();
+      if (result.empty) {
+        response.status(401);
+        response.end();
+        return;
+      }
+      const user = result.docs[0];
+  
+      await user.ref.collection("library").doc(isbn).delete();
+  
+      response.end();
+    }
+  );
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { QuoteScreen } from "./QuoteScreen";
 import { SearchView } from "./SearchView";
 import Constants from "expo-constants";
+import SwipeablePanel from "rn-swipeable-panel";
+import * as firebase from 'firebase';
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD5usI-2ccLN54HYvWrQDC58IiPci-oRy4",
+  authDomain: "kwooks.firebaseapp.com",
+  databaseURL: "https://kwooks.firebaseio.com",
+  projectId: "kwooks",
+  storageBucket: "kwooks.appspot.com",
+  messagingSenderId: "980225253473",
+  appId: "1:980225253473:web:709db0e55f9c32fa46ca20"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
+  const [panelActive, setPanelActive] = useState<boolean>(true)
   return (
     <View style={{ paddingTop: Constants.statusBarHeight, height: "100%" }}>
-      <SearchView
-        onAdd={()=>{}}
-        onClose={()=>{}}
-      />
+      <QuoteScreen quote="C'est la fucking vie!" author="someone smart" book="very good book" onNextQuoteRequested={() => {}}/>
+      <SwipeablePanel
+        fullWidth
+        openLarge
+        isActive={panelActive}
+        onClose={() => setPanelActive(false)}
+        barStyle={undefined as unknown as object}
+      >
+        <SearchView onAdd={() => {}} onClose={() => {}} />
+      </SwipeablePanel>
     </View>
   );
 }
