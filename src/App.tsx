a53c3635ee7dc7d180b2cf2910b6028fceb8b5be
Paul Rößler <roessler.paul@web.de>
2020-08-10 11:43:23 +0200

    Merge remote-tracking branch 'origin/main' into add-library-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { QuoteScreen } from "./QuoteScreen";
 import { LibraryScreen } from "./LibraryScreen"
 import Constants from "expo-constants"
 import { SearchView, mockSearchBase, Book } from "./SearchView";
+import SwipeablePanel from "rn-swipeable-panel";
+import * as firebase from 'firebase';
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD5usI-2ccLN54HYvWrQDC58IiPci-oRy4",
+  authDomain: "kwooks.firebaseapp.com",
+  databaseURL: "https://kwooks.firebaseio.com",
+  projectId: "kwooks",
+  storageBucket: "kwooks.appspot.com",
+  messagingSenderId: "980225253473",
+  appId: "1:980225253473:web:709db0e55f9c32fa46ca20"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
+  const [panelActive, setPanelActive] = useState<boolean>(true)
   return (
-    <View style={{paddingTop: Constants.statusBarHeight, height: "100%" }}>
-      {/* <QuoteScreen
-        quote="The quick brown fox jumped over the crazy frog."
-        author="Simon Knott"
-        book="Lebenswerk"
-        onNextQuoteRequested={() => {}}
-      /> */}
-      <LibraryScreen books={mockSearchBase} onOpenFilteredQuoteView={(book: Book) => {return;}}>
-
-      </LibraryScreen>
-      
+    <View style={{ paddingTop: Constants.statusBarHeight, height: "100%" }}>
+      <QuoteScreen quote="C'est la fucking vie!" author="someone smart" book="very good book" onNextQuoteRequested={() => {}}/>
+      <SwipeablePanel
+        fullWidth
+        openLarge
+        isActive={panelActive}
+        onClose={() => setPanelActive(false)}
+        barStyle={undefined as unknown as object}
+      >
+      <LibraryScreen books={mockSearchBase} onOpenFilteredQuoteView={(book: Book) => {return;}}/>
+      </SwipeablePanel>
     </View>
   );
 }
